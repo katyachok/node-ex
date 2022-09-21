53611f54c14b3ee11ed6a1ec5f30f355245bb417
KatePaulo <katefortests@outlook.com>
2022-09-21 16:33:09 +0300

    upd

---
+++ src/modules/greetings/constants/index.js
@@ -0,0 +1,3 @@
+export const getHelloMessage = name => `<h1>Hello ${name}</h1>`;
+
+export const HELLO_MESSAGE_DESCRIPTION = 'please provide name (ex. /api/greeting/someName)';
---
+++ src/modules/greetings/controllers/sayHi.js
@@ -0,0 +1,7 @@
+import { getHelloMessage, HELLO_MESSAGE_DESCRIPTION} from '../constants/index.js';
+
+export const sayHi = (req, res) => {
+	const { name } = req.params;
+	if (!name) res.status(400).json(HELLO_MESSAGE_DESCRIPTION);
+	else res.status(200).send(getHelloMessage(name));
+};
