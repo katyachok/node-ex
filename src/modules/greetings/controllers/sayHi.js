6f71514ab075dd58d0e902a88885da0c055ccedc
KatePaulo <katefortests@outlook.com>
2022-10-11 14:31:36 +0300

    upd

--- src/modules/greetings/controllers/sayHi.js
+++ src/modules/greetings/controllers/sayHi.js
@@ -2,6 +2,8 @@ import { getHelloMessage, HELLO_MESSAGE_DESCRIPTION} from '../constants/index.js
 
 export const sayHi = (req, res) => {
 	const { name } = req.params;
-	if (!name) res.status(400).json(HELLO_MESSAGE_DESCRIPTION);
-	else res.status(200).send(getHelloMessage(name));
+	if (!name) {
+		res.status(400).json(HELLO_MESSAGE_DESCRIPTION);
+	}
+	res.status(200).send(getHelloMessage(name));
 };
