2f25ebf0765a271d48df96aea384a39e5910bdd5
katyachok <katetestwo@outlook.com>
2022-11-04 16:51:28 +0300

    fixes

--- src/modules/notes/controllers/updateNote.js
+++ src/modules/notes/controllers/updateNote.js
@@ -1,22 +1,5 @@
 import { HttpStatusCode } from '#src/constants';
-import { CustomAPIError } from '#errors/custom-error';
-import { PROVIDE_VALUES } from '../constants/index.js';
-import { schemas } from '../schemas.js';
 
 export const updateNote = (req, res) => {
-	const { body } = req;
-	const noParameters = !Object.keys(body).length;
-	if (noParameters) {
-		throw new CustomAPIError(HttpStatusCode.BAD_REQUEST, PROVIDE_VALUES);
-	}
-
-	const result = schemas.noteUpdate.validate(body);
-	const { error } = result;
-	const valid = error == null;
-
-	if (!valid) {
-		throw new CustomAPIError(HttpStatusCode.UNPROCESSABLE_ENTITY, error);
-	}
-
 	return res.status(HttpStatusCode.CREATED).json(req.body);
 };
--- src/modules/notes/routes/index.js
+++ src/modules/notes/routes/index.js
@@ -4,10 +4,14 @@ import { getNotes } from '../controllers/getNotes.js';
 import { createNote } from '../controllers/createNote.js';
 import { updateNote } from '../controllers/updateNote.js';
 import { deleteNote } from '../controllers/deleteNote.js';
+import { validationMiddleware } from '../middlewares/validationMiddleware.js';
+import { noteCreateSchema, noteUpdateSchema } from '../schemas.js';
 
 const router = express.Router();
 
-router.route('/').get(getNotes).post(createNote);
-router.route('/:id').put(updateNote).delete(deleteNote);
+router.route('/').get(getNotes);
+router.post('/', validationMiddleware(noteCreateSchema), createNote);
+router.route('/:id').delete(deleteNote);
+router.put('/:id', validationMiddleware(noteUpdateSchema), updateNote);
 
 export default router;
